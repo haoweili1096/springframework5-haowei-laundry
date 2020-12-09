e343bf9294e5b15a0271c4fd8a5264b059fe1c8f
haoweili1096 <haoweili007@gmail.com>
2020-12-08 19:06:53 -0800

    Adding Index Page and Controller. closes #7.

---
+++ laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package haowei.springframework.springframework5haoweilaundry.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
