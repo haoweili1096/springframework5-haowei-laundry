7513b1b1df48da581876e10f4ed64db99b4175e1
haoweili1096 <haoweili007@gmail.com>
2020-12-08 19:16:33 -0800

    Adding Owner Index Page and Controller. closes #11.

---
+++ laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package haowei.springframework.springframework5haoweilaundry.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String ownersList(){
+        return "owners/index";
+    }
+}
