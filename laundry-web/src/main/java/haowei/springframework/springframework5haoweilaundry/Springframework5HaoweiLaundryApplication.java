2ff4cdb507e81c87bd18d8f8b6e40a4203a64e5e
haoweili1096 <haoweili007@gmail.com>
2020-12-08 17:21:03 -0800

    refactoring to multi-model maven project. closes #1.

---
+++ laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/Springframework5HaoweiLaundryApplication.java
@@ -0,0 +1,13 @@
+package haowei.springframework.springframework5haoweilaundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5HaoweiLaundryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springframework5HaoweiLaundryApplication.class, args);
+    }
+
+}
