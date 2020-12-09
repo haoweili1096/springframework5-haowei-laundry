542841192dd8ac4b836057b62ce66ba488d1a598
haoweili1096 <haoweili007@gmail.com>
2020-12-08 19:58:59 -0800

    updated base entity with JPA annotation. closes #13.

--- laundry-data/src/main/java/haowei/springframework/springframework5haoweilaundry/model/BaseEntity.java
+++ laundry-data/src/main/java/haowei/springframework/springframework5haoweilaundry/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package haowei.springframework.springframework5haoweilaundry.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+//@MappedSuperclass: established this as a base class to JPA
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
