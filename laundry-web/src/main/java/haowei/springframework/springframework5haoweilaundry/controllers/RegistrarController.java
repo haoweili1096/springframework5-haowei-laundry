96af2914df1e026d6a1aae2296f41a974ef1d333
haoweili1096 <haoweili007@gmail.com>
2020-12-08 22:35:41 -0800

    Adding Registrar List and updated Controller. closes #24.

--- laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/controllers/RegistrarController.java
+++ laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/controllers/RegistrarController.java
@@ -1,12 +1,22 @@
 package haowei.springframework.springframework5haoweilaundry.controllers;
 
+import haowei.springframework.springframework5haoweilaundry.services.RegistrarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegistrarController {
+
+    private final RegistrarService registrarService;
+
+    public RegistrarController(RegistrarService registrarService) {
+        this.registrarService = registrarService;
+    }
+
     @RequestMapping({"/registrars", "/registrars/index", "registrars/index.html"})
-    public String registrarsList(){
+    public String registrarsList(Model model){
+        model.addAttribute("registrars", registrarService.findAll());
         return "registrars/index";
     }
 }
