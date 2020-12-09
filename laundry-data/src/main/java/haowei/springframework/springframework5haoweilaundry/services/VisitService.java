3c1c378a6113d6c0ebb4417facc035d182ab283c
haoweili1096 <haoweili007@gmail.com>
2020-12-08 21:27:20 -0800

    Adding interface for Visit service. closes #21.

---
+++ laundry-data/src/main/java/haowei/springframework/springframework5haoweilaundry/services/VisitService.java
@@ -0,0 +1,7 @@
+package haowei.springframework.springframework5haoweilaundry.services;
+
+import haowei.springframework.springframework5haoweilaundry.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitService extends CrudRepository<Visit, Long> {
+}
