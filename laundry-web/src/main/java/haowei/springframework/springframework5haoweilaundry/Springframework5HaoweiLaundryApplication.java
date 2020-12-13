9ea3410aa9493657f993eb38cb46f13dc9a2a095
haoweili1096 <haoweili007@gmail.com>
2020-12-12 22:26:03 -0800

    Adding a drop list for users to choose their preferred language. closes #42.

--- laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/Springframework5HaoweiLaundryApplication.java
+++ laundry-web/src/main/java/haowei/springframework/springframework5haoweilaundry/Springframework5HaoweiLaundryApplication.java
@@ -2,12 +2,40 @@ package haowei.springframework.springframework5haoweilaundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class Springframework5HaoweiLaundryApplication {
+public class Springframework5HaoweiLaundryApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Springframework5HaoweiLaundryApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.US);
+
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
